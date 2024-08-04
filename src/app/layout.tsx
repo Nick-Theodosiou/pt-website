faa1e6fed265a440667487a10297a696aaf2e82b
Nick-Theodosiou <nicolastheodosiou2015@gmail.com>
2024-08-04 14:36:15 +0300

    Added website analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { NextUIProvider } from "@nextui-org/react";
+import { Analytics } from "@vercel/analytics/react"
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -23,6 +24,7 @@ export default function RootLayout({
       <body className={inter.className}>
         <NextUIProvider>
           {children}
+          <Analytics />
         </NextUIProvider>
       </body>
     </html>
